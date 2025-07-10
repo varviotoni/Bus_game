ef01f40b7e64673f11463e18d83014e5d167f097
hermes <tonivarvio@duck.com>
2025-07-10 14:51:41 +0300

    Modified sgame startup sequence + creategame

--- Nysse_game/Game/main.cc
+++ Nysse_game/Game/main.cc
@@ -8,12 +8,18 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(offlinedata);
 
     GameSide::StartDialog d;
+    auto icity = GameSide::createGame();
+
+    std::shared_ptr<GameSide::City> city =
+            std::dynamic_pointer_cast<GameSide::City>(icity);
 
     GameSide::MainWindow w(&d);
 
-    w.show();
 
     d.open();
+    w.show();
+
+    
 
     return a.exec();
 }
